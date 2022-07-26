02ba415c31537ae3c51e04a8072e1d5261652e93
Zain Adam <zainadam.id@gmail.com>
2022-07-26 22:02:14 +0700

    refactor: update env var

--- config/services.php
+++ config/services.php
@@ -31,9 +31,9 @@ return [
     ],
 
     'google' => [
-      'client_id' => env('GOOGLE_CLIENT_ID'),
-      'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-      'redirect' => env('GOOGLE_REDIRECT_URL')
+      'client_id' => env('GOOGLE_OAUTH_CLIENT_ID'),
+      'client_secret' => env('GOOGLE_OAUTH_CLIENT_SECRET'),
+      'redirect' => env('GOOGLE_OAUTH_REDIRECT_URL')
     ]
 
 ];
