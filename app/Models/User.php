c2bd55b9c09536258f1fdab035cec2262aea6fc5
Zain Adam <zainadam.id@gmail.com>
2022-08-03 20:45:31 +0700

    chore: update user soft delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
@@ -13,7 +14,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements CanResetPassword
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes, HasApiTokens, HasFactory, Notifiable;
 
 
 
@@ -50,24 +51,6 @@ class User extends Authenticatable implements CanResetPassword
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot()
-    {
-        parent::boot();
-
-        // auto-sets values on creation
-        static::creating(function ($query) {
-            if (! isset($query->password)) {
-                $query->attributes['password'] = bcrypt(bin2hex(random_bytes(10)));
-            }
-            
-        });
-    }
-
     public function roles()
     {
       return $this->hasManyThrough(Role::class, UserRole::class, 'user_id', 'id', 'id', 'role_id');
@@ -81,6 +64,5 @@ class User extends Authenticatable implements CanResetPassword
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = bcrypt($value);
-        $this->attributes['is_user_set_password'] = true;
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,38 +2,52 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('google_id')->unique()->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_user_set_password')->default(false);
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('email')->unique();
+      $table->string('google_id')->unique()->nullable();
+      $table->timestamp('email_verified_at')->nullable();
+      $table->string('password')->nullable();
+      $table->boolean('is_admin')->default(false);
+      $table->rememberToken();
+      $table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+      $table->string('id_number')->nullable();
+      $table->boolean('is_male')->nullable();
+      $table->string('religion')->nullable();
+      $table->string('birth_city')->nullable();
+      $table->date('birth_date')->nullable();
+      $table->text('hobby')->nullable();
+      $table->text('motto')->nullable();
+      $table->text('bio')->nullable();
+      $table->string('img_path')->nullable();
+
+      $table->softDeletes();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+    Schema::dropIfExists('users');
+    DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+  }
 }
