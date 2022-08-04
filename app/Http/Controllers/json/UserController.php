ee1efb20802b6be2a7c6de2dc31d4273cbf6269a
Zain Adam <zainadam.id@gmail.com>
2022-08-04 19:17:03 +0700

    chore: add get users via API

---
+++ app/Http/Controllers/json/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\json;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+  private function formatJsonResponse($status = null, $message = null, $data = null)
+  {
+    return response()->json([
+      'status' => $status,
+      'message' => $message,
+      'data' => $data
+    ]);
+  }
+
+  public function getUsers(Request $request)
+  {
+    if (!$request->q) {
+      return $this->formatJsonResponse('error', 'q cannot be null');
+    }
+
+    if (strlen($request->q) < 4) {
+      return $this->formatJsonResponse('error', 'q cannot less than 4 character');
+    }
+
+
+    $users = User::where('name', 'LIKE', '%' . $request->q . '%')
+      ->orwhere('id_number', 'LIKE', '%' . $request->q . '%')->get();
+
+    dd($users);
+  }
+}
